6762cbd6bf9330765369f67948e13a85b66fb3c9
antonio chiurla <antoniochiurla@gmail.com>
2021-02-18 12:02:20 +0100

    added correct implementation for rename command

--- src/main/java/org/apache/jmeter/protocol/ssh/sampler/SSHSFTPSamplerBeanInfo.java
+++ src/main/java/org/apache/jmeter/protocol/ssh/sampler/SSHSFTPSamplerBeanInfo.java
@@ -42,7 +42,8 @@ public class SSHSFTPSamplerBeanInfo extends AbstractSSHSamplerBeanInfo {
             SSHSFTPSampler.SFTP_COMMAND_PUT,
             SSHSFTPSampler.SFTP_COMMAND_RM, 
             SSHSFTPSampler.SFTP_COMMAND_RMDIR,
-            SSHSFTPSampler.SFTP_COMMAND_LS
+            SSHSFTPSampler.SFTP_COMMAND_LS,
+            SSHSFTPSampler.SFTP_COMMAND_RENAME
         });
         
         p = property("source"); // $NON-NLS-1$
